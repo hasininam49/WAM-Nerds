e396684e466226cae40f5b4001e4ea79e1b1acfe
TheWAMnerd <thewamnerds@gmail.com>
2023-06-10 15:08:36 -0400

    YAYAYAYYAY

--- docs/Share.js
+++ docs/Share.js
@@ -1,18 +1,10 @@
-<?php
-
-// Switch to HTML mode
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="Main.css">
-</head>
-<body>
+// This is the shared code
+var sharedCode = `
 <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-        <a class="navbar-brand Nav_Text_A" href="#" style="color: aliceblue;" id="Branding">
+        <a class="navbar-brand Nav_Text_A" href="index.html" style="color:aliceblue;" id="Branding">
             <img src="favicon.ico" width="50px" height="50px" id="NavLogo">
-            The WAM Nerds
+            TheWAMnerds
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -20,13 +12,15 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link Nav_Text_A" aria-current="page" href="#">Home</a>
+                    <a class="nav-link Nav_Text_A" href="#">Join Us!</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link Nav_Text_A" href="aboutus.html">About us</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle Nav_Text_A" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Content</a>
+                    <a class="nav-link dropdown-toggle Nav_Text_A" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: aliceblue;">
+                        Content
+                    </a>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="#">Book Reviews</a></li>
                         <li><a class="dropdown-item" href="#">School blogs</a></li>
@@ -35,16 +29,15 @@
                     </ul>
                 </li>
             </ul>
-            <form class="d-flex" role="search" id="Search_Engine">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button id="SearchButton" type="submit">Search</button>
-            </form>
         </div>
     </div>
-</nav>
-</body>
-<?php
+</nav>`;
 
-?>
-  
+// Function to load the shared code into the specified element
+function loadSharedCode() {
+    var placeholder = document.getElementById('MyNa');
+    placeholder.innerHTML = sharedCode;
+}
 
+// Call the function to load the shared code when the DOM is ready
+document.addEventListener('DOMContentLoaded', loadSharedCode);
