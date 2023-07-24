45d13d6518d04e0fafe2aa2acb0803063a295f0a
TheWAMnerd <thewamnerds@gmail.com>
2023-07-24 17:22:48 -0400

    hmmm

--- root/src/main.js
+++ root/src/main.js
@@ -11,20 +11,20 @@ else {
 }
 
 const { Client, Account, ID } = Appwrite;
+const { Client, Databases, Query } = Appwrite;
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
     .setProject('64be9ef6649824b39e93');               // Your project ID
 
 const account = new Account(client);
 
-// Register User
+
 // Register User
 account.create({
     id: ID.unique(),
     email: 'thewamnerds@gmail.com',
     password: 'WAMnerd@49',
-    name: 'Hasini Namaduru',
-    domain: 'cloud.appwrite.io'
+    name: 'Hasini Namaduru'
 }).then(response => {
     console.log(response);
 }, error => {
@@ -32,22 +32,45 @@ account.create({
 });
 
 client.subscribe('files', response => {
-    if (response.events.includes('buckets.*.files.*.create')) {
-        // Get the data of the new document
-        const document = response.payload;
-
-        // Retrieve each separate attribute of the document
-        const name = document.Author;
-        const Title = document.Title;
-        const email = document.Email;
-        const DateTime = document.Date-Time ;
-        const content = document.Content;
-        const keypoints = document.KeyPoints;
-
-        // Log the data of the new document
-        console.log(name, Title, email,DateTime,content,keypoints);
+    if(response.events.includes('buckets.*.files.*.create')) {
+        // Log when a new file is uploaded
+        console.log(response.payload);
     }
-    else{
-        console.log("not working");
+});
+
+
+const databases = new Databases(client);
+
+database.listDocuments('collection', [
+    Query.equal('title', 'Avatar'),
+    Query.orderBy('createdAt', 'desc'),
+    Query.select([
+        'Author',
+        'Title',
+        'Date-Time',
+        'Email',
+        'Content',
+        'KeyPoints'
+    ])
+], {
+    limit: 10,
+    offset: 0
+}).then(response => {
+    if (response.documents) {
+        response.documents.forEach(document => {
+            const author = document.Author;
+            const title = document.Title;
+            const dateTime = document.Date-Time;
+            const email = document.Email;
+            const content = document.Content;
+            const keyPoints = document.KeyPoints;
+
+            console.log(author, title, dateTime, email, content, keyPoints);
+        });
     }
+    else {
+        console.log('No documents found');
+    }
+}).catch(error => {
+    console.log(error);
 });
