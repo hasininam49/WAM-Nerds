77ccdd865530943253a6f4eaa8c43b269f407860
TheWAMnerd <thewamnerds@gmail.com>
2023-07-25 13:39:27 -0400

    LESGOooooooooooooo

--- root/src/main.js
+++ root/src/main.js
@@ -10,6 +10,8 @@ else {
     console.log("Already alerted");
 }
 
+
+
 const { Client, Databases, Query } = Appwrite;
 
 const client = new Client()
@@ -18,45 +20,38 @@ const client = new Client()
 
 const databases = new Databases();
 
+const collectionId = '64bea007467268695d2a'; // Replace 'your-collection-id' with the actual collection ID
 
-database.listDocuments('collection', [
-    Query.equal('title', 'Avatar'),
-    Query.orderBy('createdAt', 'desc'),
+databases.listDocuments(collectionId, [
+    Query.equal('title', 'Avatar'), // Use Query.equal() for equal conditions
     Query.select([
         'Author',
         'Title',
-        'Date-Time',
+        'Date_Time', // Change to camelCase or any valid variable name
         'Email',
         'Content',
         'KeyPoints'
     ])
 ], {
     limit: 10,
-    offset: 0
+    offset: 0,
+    orderField: 'createdAt', // Specify the field to be used for sorting
+    orderType: 'desc' // Specify the sorting order, 'desc' for descending, 'asc' for ascending
 }).then(response => {
     if (response.documents) {
         response.documents.forEach(document => {
             const author = document.Author;
             const title = document.Title;
-            const dateTime = document.Date-Time;
+            const dateTime = document['Date_Time']; // Use square brackets to access properties with hyphens
             const email = document.Email;
             const content = document.Content;
             const keyPoints = document.KeyPoints;
 
             console.log(author, title, dateTime, email, content, keyPoints);
         });
-    }
-    else {
+    } else {
         console.log('No documents found');
     }
 }).catch(error => {
     console.log(error);
-});
-
-
-client.subscribe('files', response => {
-    if(response.events.includes('buckets.*.files.*.create')) {
-        // Log when a new file is uploaded
-        console.log(response.payload);
-    }
 });
