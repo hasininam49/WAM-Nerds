ca75fd5f7aad6d47023d78d38f8441ccd96e96eb
TheWAMnerd <thewamnerds@gmail.com>
2023-08-11 17:00:39 -0400

    idek

--- root/src/main.js
+++ root/src/main.js
@@ -24,3 +24,31 @@ client.subscribe('files', response => {
         console.log(response.payload);
     }
 });
+
+const apiKey = 'd2cd026e1305d726ca0b3ca6abeb96544a2bee9bf52020bad9e73664a23bd851360db305158f50bccf93d14e0a95daf7917f0bd2736082532e6e9f6b92577be053766995ad2554adb58c204e7a7bbde26799a0851d750f2d867f8de26b30585fb6b07cd6a0b176bac32c48a67c1dd844d182f225035c917419e69866882d8b5e'; // Replace with your actual API key
+const collectionId = '64bea007467268695d2a'; // Replace with your collection ID
+const buttonContainer = document.getElementById('button-container');
+const socket = new WebSocket('wss://api.appwrite.io/v1/live');
+
+socket.onopen = () => {
+  socket.send(JSON.stringify({ "key": apiKey }));
+};
+
+socket.onmessage = event => {
+  const message = JSON.parse(event.data);
+  
+  if (message.event === 'documents.create') {
+    const newDocument = message.payload;
+    createButton(newDocument);
+  }
+};
+
+function createButton(document) {
+  const button = document.createElement('button');
+  button.textContent = document.title; // Assuming you have a 'title' field in your document
+  button.addEventListener('click', () => {
+    // You can use 'document' data for further actions if needed
+    console.log('Button clicked with data:', document);
+  });
+  buttonContainer.appendChild(button);
+}
