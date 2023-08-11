b26987e70674c6e4a3419c938efd0b65235e627f
TheWAMnerd <thewamnerds@gmail.com>
2023-08-11 14:50:23 -0400

    yayay
    FNIALLY DID IT

--- root/src/main.js
+++ root/src/main.js
@@ -14,7 +14,7 @@ const { Client, Account, ID } = Appwrite;
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-    .setProject('[64be9ef6649824b39e93]');               // Your project ID 
+    .setProject('64be9ef6649824b39e93');               // Your project ID 
 
  
 // Subscribe to files channel
